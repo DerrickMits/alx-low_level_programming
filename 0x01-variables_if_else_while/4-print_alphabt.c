49ac392a3286e7f974fe530633d3e29923a88f08
DerrickMits <derrickodiwuor@gmail.com>
2023-10-06 08:17:59 -0700

    Exclude some letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 /**
- * main - Exclude some letters
- * Return: 0 (success)
+ * main - Print to exclude some letters
+ * Return: 0 (Success)
  */
 int main(void)
 {
-int n = 97;
-while (n <= 122)
+char letter = 'a';
+while (letter <= 'z')
 {
-if (n == 101 || n == 113)
+if (letter != 'e' && letter != 'q')
 {
-n++;
-continue;
+putchar(letter);
 }
-putchar("\n");
+letter++;
 }
-n++;
+putchar('\n');
 return (0);
 }
