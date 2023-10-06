d0215a8d24a10406c89edb96678768b250dc0925
DerrickMits <derrickodiwuor@gmail.com>
2023-10-06 06:52:49 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,13 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d and is ", n, abs(n % 10));
-if (n % 10 > 5)
+int lastDigit = abs(n % 10); // Calculate the absolute value of the last digit
+printf("Last digit of %d is %d%d and is", n, (n < 0 ? -1 : 1), lastDigit);
+if (lastDigit > 5)
 {
 printf("greater than 5\n");
 }
-else if (n % 10 == 0)
+else if (lastDigit == 0)
 {
 printf("0\n");
 }
