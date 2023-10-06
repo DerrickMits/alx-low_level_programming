1615761a280f6dc4674362dcaabfe465b1cb472c
DerrickMits <derrickodiwuor@gmail.com>
2023-10-06 07:56:07 -0700

    Exlude some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Exclude some letters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+if (n == 101 || n == 113)
+{
+continue;
+putchar("\n");
+n++;
+return (0);
+}
