6a534d04a9b9c54c20d2e18580be33abfb7d7fe9
DerrickMits <derrickodiwuor@gmail.com>
2023-10-11 11:22:06 -0700

    List of natural numbers below 10

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - List of natural numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int limit = 1024;
+int sum = 0;
+for (int i = 1; i < limit; i++) 
+{
+if (i % 3 == 0 || i % 5 == 0) 
+{
+sum += i;
+}
+}
+printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
+return (0);
+}
