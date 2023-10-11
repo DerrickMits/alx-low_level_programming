c60cd84bd57c9a422ab8a8fd4c39853498213692
DerrickMits <derrickodiwuor@gmail.com>
2023-10-11 07:23:03 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,29 +9,45 @@ void print_to_98(int n)
 int i;
 if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+for (i = n; i < 98; i++)
 {
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-if (i != 98)
-{
+print_number(i);
 _putchar(',');
 _putchar(' ');
-}
 }
 }
 else
 {
-for (i = n; i >= 98; i--)
+for (i = n; i > 98; i--)
 {
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-if (i != 98)
-{
+print_number(i);
 _putchar(',');
 _putchar(' ');
 }
 }
-}
+print_number(98);
 _putchar('\n');
+}
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ */
+void print_number(int n)
+{
+if (n == -2147483648)
+{
+_putchar('-');
+_putchar('2');
+print_number(147483648);
+}
+else if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10 != 0)
+{
+print_number(n / 10);
+}
+ _putchar((n % 10) + '0');
 }
