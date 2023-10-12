635567ed691cff5db3dbd7a4adc9931e2764831f
DerrickMits <derrickodiwuor@gmail.com>
2023-10-12 02:14:06 -0700

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, the number of characters written is returned.
+ * On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+return write(1, &c, 1);
+}
