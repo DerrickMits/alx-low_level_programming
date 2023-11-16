9ad7bef54a0994e11691bb26ceaec8fde0e9768c
DerrickMits <derrickodiwuor@gmail.com>
2023-11-16 02:20:14 -0800

    Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,18 @@
 section .data
-hello db 'Hello, Holberton', 0
-newline db 10  ; ASCII code for newline
+    hello db 'Hello, Holberton', 0
+    newline db 10  ; ASCII code for newline
+
 section .text
-global main
-extern printf
-section .text
-main:
-push rdi; Push a register to maintain stack alignment
-mov rdi, hello; Load the address of the string into rdi
-call printf; Call printf function
-pop rdi; Restore the stack pointer
-mov eax, 0; Return 0 from main
-ret
+    global main
+
+    extern printf
+    section .text
+    main:
+        push rdi                ; Push a register to maintain stack alignment
+        mov rdi, hello          ; Load the address of the string into rdi
+        call printf             ; Call printf function for the first part
+        mov rdi, newline        ; Load the address of the newline character into rdi
+        call printf             ; Call printf function for the newline
+        pop rdi                 ; Restore the stack pointer
+        mov eax, 0              ; Return 0 from main
+        ret
