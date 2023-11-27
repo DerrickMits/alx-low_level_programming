d6d266a8709510d09ca6a34c657e4d29d7b99030
DerrickMits <derrickodiwuor@gmail.com>
2023-11-27 10:15:01 -0800

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,7 +44,7 @@ dprintf(STDERROR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 error_file(file_from, file_to, argv[1]);
 ch = 1024;
 while (ch == 1024)
