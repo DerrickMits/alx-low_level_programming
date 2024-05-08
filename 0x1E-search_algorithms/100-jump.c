1e4b16a23d682d824ccc1252218f683503ef65b8
DerrickMits <derrickodiwuor@gmail.com>
2024-05-08 02:35:48 -0700

    function that searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -24,7 +24,7 @@ if (array[index] == value)
 return (index);
 k++;
 prev = index;
-index = k * m;
+index = k *m;
 } while (index < (int)size && array[index] < value);
 printf("Value found between indexes [%d] and [%d]\n", prev, index);
 for (; prev <= index && prev < (int)size; prev++)
